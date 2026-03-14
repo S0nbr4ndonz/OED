ed701aeb2acc03b73ddee7f87888582afac79a5f
trivankute <trivan7a16@gmail.com>
2026-03-14 15:24:08 -0700

    Added BG22 bar group flow test (Passed)

--- src/server/test/web/readingsBarGroupFlow.js
+++ src/server/test/web/readingsBarGroupFlow.js
@@ -11,6 +11,7 @@ const { chai, mocha, app } = require('../common');
 const Unit = require('../../models/Unit');
 const { prepareTest,
 	parseExpectedCsv,
+	createTimeString,
 	expectReadingToEqualExpected,
 	getUnitId,
 	ETERNITY,
@@ -572,7 +573,99 @@ mocha.describe('readings API', () => {
 					// Check that the API reading is equal to what it is expected to equal
 					expectReadingToEqualExpected(res, expected, GROUP_ID);
 				})
+
 				// Add BG22 here
+				mocha.it('BG22: 13 day bars (no values) for 15 + 20 minute reading intervals and flow units with reduced, partial days & kW as kW', async () => {
+					const unitDatakW = [
+						{ 
+							// u4
+							name: 'kW', 
+							identifier: '', 
+							unitRepresent: Unit.unitRepresentType.FLOW, 
+							secInRate: 3600, 
+							typeOfUnit: Unit.unitType.UNIT, 
+							suffix: '', 
+							displayable: Unit.displayableType.ALL, 
+							preferredDisplay: true, 
+							note: 'kilowatts' 
+						},
+						{ 
+							// u5
+							name: 'Electric', 
+							identifier: '', 
+							unitRepresent: Unit.unitRepresentType.FLOW, 
+							secInRate: 3600, 
+							typeOfUnit: Unit.unitType.METER, 
+							suffix: '', 
+							displayable: Unit.displayableType.NONE, 
+							preferredDisplay: false, 
+							note: 'special unit' 
+						},
+					];
+					const conversionDatakW = [
+						{ 
+							// c4
+							sourceName: 'Electric', 
+							destinationName: 'kW', 
+							bidirectional: false, 
+							slope: 1, 
+							intercept: 0, 
+							note: 'Electric → kW' 
+						}
+					];
+					const meterDatakWGroups = [
+						{
+							name: 'meterDatakWGroups',
+							unit: 'Electric',
+							defaultGraphicUnit: 'kW',
+							displayable: true,
+							gps: undefined,
+							note: 'special meter',
+							file: 'test/web/readingsData/readings_ri_15_days_75.csv',
+							deleteFile: false,
+							readingFrequency: '15 minutes',
+							id: METER_ID
+						},
+						{
+							name: 'meterDatakWOther',
+							unit: 'Electric',
+							defaultGraphicUnit: 'kW',
+							displayable: true, 
+							gps: undefined,
+							note: 'special meter',
+							file: 'test/web/readingsData/readings_ri_20_days_75.csv',
+							deleteFile: false,
+							readingFrequency: '20 minutes',
+							id: (METER_ID + 1)
+						}
+						];
+						const groupDatakW = [
+							{
+								id: GROUP_ID,
+								name: 'meterDatakWGroups + meterDatakWOther',
+								displayable: true,
+								note: 'special group',
+								defaultGraphicUnit: 'kW',
+								childMeters: ['meterDatakWGroups', 'meterDatakWOther'],
+								childGroups: [],
+							}
+						]
+						
+					//load data into database
+					await prepareTest(unitDatakW, conversionDatakW, meterDatakWGroups, groupDatakW);
+					//get unit ID since the DB could use any value.
+					const unitId = await getUnitId('kW');
+					// Load the expected response data from the corresponding csv file
+					const expected = await parseExpectedCsv('src/server/test/web/readingsData/expected_bar_group_ri_15-20_mu_kW_gu_kW_st_2022-08-2013#18#28_bd_13.csv');
+					// Create a request to the API for unbounded reading times and save the response
+					const res = await chai.request(app).get(`/api/unitReadings/bar/groups/${GROUP_ID}`)
+						.query({ 
+							timeInterval: createTimeString('2022-08-20', '07:25:35', '2022-10-28', '13:18:28'),
+							barWidthDays: '13',
+							graphicUnitId: unitId });
+					// Check that the API reading is equal to what it is expected to equal
+					expectReadingToEqualExpected(res, expected, GROUP_ID);
+				})
 			});
 		});
 	});
