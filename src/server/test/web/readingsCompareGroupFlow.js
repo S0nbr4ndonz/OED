c116128a650e29793dc2b5f80cd57999df5df6dc
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-06-07 19:46:59 -0700

    Fixed error by recopying CG15 code

--- src/server/test/web/readingsCompareGroupFlow.js
+++ src/server/test/web/readingsCompareGroupFlow.js
@@ -311,7 +311,7 @@ mocha.describe('readings API', () => {
                 // Add CG18 here
 
                 mocha.it('CG19: 7 day shift end 2022-11-01 15:00:00 (beyond data) for 15 minute reading intervals and flow units & kW as kW', async () => {
-                    // unit data
+                     // unit data
 					const unitDatakW = [
                         { 
                             // u4
